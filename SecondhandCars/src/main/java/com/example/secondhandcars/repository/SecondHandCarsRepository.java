5c2048df7fe661b1ade3d3be73a49978d69e81d2
Rishvakrish <58131992+Rishvakrish@users.noreply.github.com>
2023-06-06 11:20:45 +0530

    Add files via upload

---
+++ SecondhandCars/src/main/java/com/example/secondhandcars/repository/SecondHandCarsRepository.java
@@ -0,0 +1,36 @@
+package com.example.secondhandcars.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.secondhandcars.model.SecondHandCars;
+
+public interface SecondHandCarsRepository extends JpaRepository<SecondHandCars,Integer>{
+		
+		public List<SecondHandCars>findByBrandStartingWith(String prefix);
+		public List<SecondHandCars>findByBrandEndingWith(String suffix);
+		public List<SecondHandCars>findByModel(String model);
+
+		//positional parameter
+		@Query("select u from SecondHandCars u where u.brand=?1 and u.colour=?2")
+		public List<SecondHandCars> fetchPositionalParameter(String brand,String colour);
+		
+		//named parameter
+			@Query("select u from SecondHandCars u where u.brand=?1")
+			public List<SecondHandCars> fetchNamedParameter(String brand);	
+			
+		//DML Delete Query
+			@Modifying
+			@Query("delete from SecondHandCars s where s.model=?1")
+			public int deleteCarByModel(String model);
+			
+		//update query
+			@Modifying
+			@Query("update  SecondHandCars s set s.colour=?1 where s.model=?2")
+			public int updateCarByModel(String colour,String model);
+			
+		
+}
